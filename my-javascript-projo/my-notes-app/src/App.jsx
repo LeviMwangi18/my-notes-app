21b83a85d5b4c966da7aa754680984b861bf978c
[LeviMwangi18] <[mungailevi12gmail.com]>
2025-03-09 17:07:35 +0300

    updated

--- my-javascript-projo/my-notes-app/src/App.jsx
+++ my-javascript-projo/my-notes-app/src/App.jsx
@@ -1,9 +1,12 @@
-import Header from './Header.jsx'
-
+import Header from './assets/Header.jsx'
+import Hero from './assets/Hero.jsx'
 function App() {
   return(
+    <>
     <Header/>
+    <Hero/>
+    </>
   )
 }
 
-export default App
+export default App;
--- my-javascript-projo/my-notes-app/src/assets/Header.jsx
+++ my-javascript-projo/my-notes-app/src/assets/Header.jsx
@@ -7,7 +7,7 @@ function Header(){
             </h1>
             <div className="search-bar" class="bg-slate-300 p-1 flex gap-3 items-center br-10">
                 <i class="fa-solid fa-search"></i>
-                <input type="text" placeholder="search" class="placeholder:text-md placeholder:text-zinc-950"/>
+                <input type="text" placeholder="search" class="placeholder:text-md placeholder:text-zinc-950 bo"/>
             </div>
             <div className="user">
                 <i class="fa-solid fa-user"></i>
