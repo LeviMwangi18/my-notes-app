8b1f6eba54a66dd82bc2668399b4996ab22eadfa
[LeviMwangi18] <[mungailevi12gmail.com]>
2025-03-09 16:22:27 +0300

    first-commit

---
+++ my-javascript-projo/my-notes-app/src/Header.jsx
@@ -0,0 +1,19 @@
+
+function Header(){
+    return(
+        <header class="flex p-5 bg-orange-500 justify-evenly items-center">
+            <h1 class="text-2xl font-extrabold">
+               My Notes
+            </h1>
+            <div className="search-bar" class="bg-slate-300 p-1 flex gap-3 items-center br-10">
+                <i class="fa-solid fa-search"></i>
+                <input type="text" placeholder="search" class="placeholder:text-md placeholder:text-zinc-950"/>
+            </div>
+            <div className="user">
+                <i class="fa-solid fa-user"></i>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
